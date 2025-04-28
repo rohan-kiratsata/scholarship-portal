b55f131196ca7c03b4a655fc8a358164537c358d
rohan-kiratsata <kiratsatarohan1234@gmail.com>
2025-04-28 23:12:59 +0530

    builfd fix tesr

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,10 +1,28 @@
 import admin from "firebase-admin";
 
-var serviceAccount = require("../scholarship-52560-firebase-adminsdk-fbsvc-8ad88709cd.json");
+// Use environment variables instead of local JSON file
+const serviceAccount = {
+  type: process.env.FIREBASE_ADMIN_TYPE || "service_account",
+  project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+  auth_uri:
+    process.env.FIREBASE_ADMIN_AUTH_URI ||
+    "https://accounts.google.com/o/oauth2/auth",
+  token_uri:
+    process.env.FIREBASE_ADMIN_TOKEN_URI ||
+    "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url:
+    process.env.FIREBASE_ADMIN_AUTH_PROVIDER_CERT_URL ||
+    "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_CERT_URL,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   });
 }
 
